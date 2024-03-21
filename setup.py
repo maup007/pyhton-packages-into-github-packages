a70dd37775065ecf8eb84d77479f857c588ddcd6
maup007 <159116868+maup007@users.noreply.github.com>
2024-03-21 19:32:27 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-  name='hello-world-package',
-  version='0.1',
-  packages=[''],
-  description='sample python package to print "Hello, world!"',
-  author='maup007',
-  author_email='maup@tdc.dk',
-  url='https://pypi.pkg.github.com/maup007/pyhton-packages-into-github-package',
+    name='hello_world',
+    version='1.0.0',
+    packages=find_packages(),
+    author='maup007',
+    author_email='maup@tdc.dk',
+    description='helloworld package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/maup007/pyhton-packages-into-github-packages',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
 )
